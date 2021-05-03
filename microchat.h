ba5b225e4e209cb27e543adeef0731f973e9035c
Karl Zeilhofer <Karl.Zeilhofer@htl.steyr.ac.at>
2021-05-03 14:57:54 +0200

    erste lauffähige version

---
+++ microchat.h
@@ -0,0 +1,47 @@
+#ifndef MICROCHAT_H
+#define MICROCHAT_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QDialogButtonBox;
+class QLabel;
+class QPushButton;
+class QTimer;
+class QUdpSocket;
+QT_END_NAMESPACE
+
+namespace Ui {
+class MicroChat;
+}
+
+class MicroChat : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MicroChat(QWidget *parent = nullptr);
+    ~MicroChat();
+
+    void transmit(QString user, QString message);
+
+
+    QUdpSocket* udpSenderSocket = 0;
+    QUdpSocket* udpReceiverSocket = 0;
+
+    QString userName = "mmuster";
+    const uint16_t Port = 1111;
+
+
+signals:
+
+public slots:
+private slots:
+    void on_pushButton_send_clicked();
+    void on_actionSend_triggered();
+    void processPendingDatagrams();
+
+private:
+    Ui::MicroChat *ui;
+};
+
+#endif // MICROCHAT_H
